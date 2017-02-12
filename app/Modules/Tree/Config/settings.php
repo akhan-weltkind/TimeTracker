cd2908d87ae06f4c3a050ef8e5aec3813313649e
Александр Хан <axiles94@gmail.com>
2017-02-12 09:37:20 +0600

    Initial clean cms

---
+++ app/Modules/Tree/Config/settings.php
@@ -0,0 +1,13 @@
+<?php
+return [
+
+    'title'=>trans('tree::admin.title'),
+    'localization'=>true,
+    "modules"=>[""=>"",
+        "news"=>trans('news::admin.title'),
+        "articles"=>trans('articles::admin.title'),
+        "gallery"=>trans('gallery::admin.title'),
+        "feedback"=>trans('feedback::admin.title'),
+        "redirector"=>trans('redirector::admin.title')],
+    "templates"=> ["inner"=>trans('tree::admin.templates.inner'), "index"=>trans('tree::admin.templates.index')]
+];
